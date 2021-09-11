7e37728937a4bcc6f693d75590c539cd4df144c4
Siarhei Sivazhalezau <ssivazhalezau@exadel.com>
2021-09-11 12:04:58 +0400

    ident changed to 2 spaces

--- tasks/task01/narcissistic.py
+++ tasks/task01/narcissistic.py
@@ -1,20 +1,20 @@
 base = 3
 
 def digits(n):
-    # return list of digits of number
-    a = []
-    if n < 10:
-        a = [n]
-    else:
-        while n:
-            n, remainder = divmod(n, 10)
-            a.append(remainder)
-    return a
+  # return list of digits of number
+  a = []
+  if n < 10:
+    a = [n]
+  else:
+    while n:
+      n, remainder = divmod(n, 10)
+      a.append(remainder)
+  return a
 
 for i in range (1, 1001):
-    s = 0
-    for d in digits(i):
-        s = s + d ** base
+  s = 0
+  for d in digits(i):
+    s = s + d ** base
 
-    if i == s:
-        print('Narcissistic: ', i,)
+  if i == s:
+    print('Narcissistic: ', i,)
