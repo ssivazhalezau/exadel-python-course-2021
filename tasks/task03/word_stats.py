daf783f7efec715f7f97e4c00d340ac13f00faf3
Siarhei Sivazhalezau <ssivazhalezau@exadel.com>
2021-09-20 14:12:27 +0400

    added task 3

---
+++ tasks/task03/word_stats.py
@@ -0,0 +1,26 @@
+import re
+
+texts = [
+   "Hello, World!",
+   "The world is mine",
+   "Hello, how are you?"
+]
+
+words =[]
+
+for i, sentence in enumerate(texts):
+    index = i
+    for w in re.sub("[^\w]", " ", str.lower(sentence)).split():
+        dict_index = next((i for i, item in enumerate(words) if item['word'] == w), None)
+        if dict_index is None:
+            words.append({'word':w, 'count':1, 'index':i})
+
+        else:
+            # print(dict_index)
+            words[dict_index]['count'] = words[dict_index]['count'] + 1
+            # print(words[dict_index]['count'])
+
+print('word    count   first line')
+for i, word in enumerate(words):
+    # print(f"{words[i]['word']<10}")
+    print(words[i]['word'].ljust(7), str(words[i]['count']).ljust(7), words[i]['index'])
