d785f2a998305c0bdca2d5bf6ae2fa02d1fb46b3
Siarhei Sivazhalezau <ssivazhalezau@exadel.com>
2021-10-03 14:17:25 +0400

    added assert

--- tasks/task05/user_factory.py
+++ tasks/task05/user_factory.py
@@ -2,13 +2,11 @@ import json
 from typing import Dict
 
 def create_user(name: str, surname: str, age: int = 42, **kwargs) -> Dict:
-    result = {  'name':     '',
-                'surname':  '',
-                'age':      65,
-                'extra':    {}
+    result = {  'name':     name,
+                'surname':  surname,
+                'age':      age,
+                'extra':    kwargs
                 }
-
-
 
     result['name'] = name
     result['surname'] = surname
@@ -18,6 +16,29 @@ def create_user(name: str, surname: str, age: int = 42, **kwargs) -> Dict:
 
     return result
 
-# print(create_user("John", "Doe"))
-# print(create_user("Bill", "Gates", age=65))
+assert create_user("John", "Doe") == \
+   {
+       "name": "John",
+       "surname": "Doe",
+       "age": 42 ,
+       "extra": {}
+   } \
+       and create_user("Bill", "Gates", age=65) == \
+   {
+       "name": "Bill",
+       "surname": "Gates",
+       "age": 65,
+       "extra": {}
+   } \
+       and create_user("Marie", "Curie", age=66, occupation="physicist", won_nobel=True) == \
+   {
+       "name": "Marie",
+       "surname": "Curie",
+       "age": 66,
+       "extra": {
+           "occupation": "physicist",
+           "won_nobel": True
+       }
+   }, 'wrong function result'
+
 print(create_user("Marie", "Curie", age=66, occupation="physicist", won_nobel=True))
